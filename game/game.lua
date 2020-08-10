67c772b594855fa6e024fc27a3ae929209d10c80
Seb <sdunne828@gmail.com>
2020-08-10 17:47:46 -0400

    added enemy spawn + collision

--- game/game.lua
+++ game/game.lua
@@ -14,11 +14,7 @@ local ammoDisplay = {}
 
 local map
 local environment = {}
-
-local sceneGroup
-local backGroup
-local mainGroup
-local uiGroup
+local enemies = {}
 
 ------------------------ Utility functions ------------------------
 
@@ -31,12 +27,17 @@ end
 
 -- Fires guns that don't have projectiles
 function shootNonProjectile( shotAngle )
-  --Initialize the shot
-  local shot = display.newPolygon( player.x, player.y, settings.guns[player.gun].hitbox )
+  --Load the shot
+  local shot = display.newPolygon( player.x, player.y, settings.guns[player.gun].shape )
   shot:setFillColor( 1, 0, 0 )
   shot.rotation = toDeg( shotAngle )
   shot.anchorX = 0
   camera:add(shot, 1)
+
+  physics.addBody( shot, "dynamic", { shape = settings.guns[player.gun].hitbox } )
+  shot.isSensor = true
+  shot.collision = onBulletCollision
+  shot:addEventListener( "collision" )
 
   player.rotation = toDeg( shotAngle ) -- Set player rotation
 
@@ -45,6 +46,22 @@ function shootNonProjectile( shotAngle )
     shot:removeSelf()
     shot = nil
   end )
+end
+
+ -- Spawns enemy in game
+function spawnEnemy()
+  local spawnLocation = environment.enemySpawns[ math.random( #environment.enemySpawns ) ]
+  local enemy = display.newRect( spawnLocation.x, spawnLocation.y, settings.enemies.enemyName.size, settings.enemies.enemyName.size )
+  enemy.type = "enemy"
+  enemy.slot = #enemies + 1
+
+  camera:add( enemy, 1 )
+
+  physics.addBody( enemy, "dynamic", {bounce = 0} )
+  enemy.linearDamping = settings.player.friction
+  enemy.angularDamping = settings.player.friction
+
+  table.insert( enemies, enemy )
 end
 
 ------------------------ Event Functions ------------------------
@@ -70,16 +87,15 @@ function shoot( event )
     local yForce = math.sin( angle ) * settings.guns[player.gun].recoil * -1
     player:applyLinearImpulse( xForce, yForce, player.x, player.y )
 
-    --
+    -- Disable shooting
     player.readyToFire = false
-    if(player.ammo > 1)then
+    if ( player.ammo > 1 ) then
        ammoDisplay[player.ammo].isVisible = false
        player.ammo = player.ammo - 1
        timer.performWithDelay( settings.guns[player.gun].attackSpeed, function()
          player.readyToFire = true
        end )
     else -- reloading
-        print( "reloading ... " )
         ammoDisplay[player.ammo].isVisible = false
         timer.performWithDelay( settings.guns[player.gun].reloadTime, function()
          player.ammo = settings.guns[player.gun].magazine
@@ -87,9 +103,19 @@ function shoot( event )
          for i = 1, player.ammo, 1 do
             ammoDisplay[i].isVisible = true
          end
-         print( "reloaded!" )
        end )
     end
+  end
+end
+
+-- Handle bullet collision
+function onBulletCollision( self, event )
+  if ( event.other.type == "enemy" ) then
+    local enemy = event.other
+    local slot = enemy.slot
+
+    enemy:removeSelf()
+    table.remove( enemies, slot )
   end
 end
 
@@ -97,12 +123,22 @@ end
 
 -- create()
 function scene:create( event )
-  -- Initialise settings
+  -- Load settings
   settings = {
+    game = {
+      spawnTime = 5000
+    },
+
     player = {
       size = 125,
       defaultGun = "shotgun",
       friction = 3
+    },
+
+    enemies = {
+      enemyName = {
+        size = 125
+      }
     },
 
     guns = {
@@ -112,7 +148,8 @@ function scene:create( event )
         magazine = 6,
         reloadTime = 2000, -- miliseconds
         attackSpeed = 500, -- minimum time between shots
-        hitbox = {0,0, 500,-300, 500,300},
+        shape = {0,0, 500,-300, 500,300},
+        hitbox = {-250,0, 250,-300, 250,300},
         activeTime = 200,
         ammoIcon = "Assets/Pixel/weaponAssets/shotgunAmmo.png",
         ammoIconSize = {width = 50, height = 132}, -- vertical image, 2.65 ratio
@@ -136,14 +173,14 @@ function scene:create( event )
   -- Setup the camera
   camera = perspective.createView()
 
-  --Initialize the map
+  --Load the map
   map = display.newRect( 0, 0, 1080, 1920 )
   map.anchorX = 0
   map.anchorY = 0
   map:setFillColor( 0.2, 0.2, 0.2 )
   camera:add( map, 2 )
 
-  -- Initialize environment
+  -- Load environment
   -- Temporary Border
   environment.topWall = display.newRect( 0, 0, 2500, 100 )
   environment.leftWall = display.newRect( 25, display.contentCenterY, 100, 1920 )
@@ -160,14 +197,21 @@ function scene:create( event )
   camera:add( environment.rightWall, 2 )
   camera:add( environment.bottomWall, 2 )
 
-  -- Initialize the player
+  -- Load enemy spawns
+  environment.enemySpawns = {
+    { x = 600, y = 200 },
+    { x = 400, y = 400 },
+    { x = 800, y = 1500}
+  }
+
+  -- Load the player
   player = display.newRect( display.contentCenterX, display.contentCenterY,
     settings.player.size, settings.player.size )
   player.gun = settings.player.defaultGun
   player.readyToFire = true
   player.ammo = settings.guns[player.gun].magazine
 
-  physics.addBody( player, "dynamic", {friction = settings.player.friction, bounce = 0} )
+  physics.addBody( player, "dynamic", {bounce = 0} )
   player.linearDamping = settings.player.friction
   player.angularDamping = settings.player.friction
 
@@ -178,7 +222,7 @@ function scene:create( event )
   camera:setFocus( player )
   camera:track()
 
-  -- Initialize the ammo count
+  -- Load the ammo count
   for i = 1, player.ammo, 1 do
     ammoDisplay[i] = display.newImageRect( settings.guns[player.gun].ammoIcon, settings.guns[player.gun].ammoIconSize.width, settings.guns[player.gun].ammoIconSize.height )
     ammoDisplay[i].x = settings.ui.ammoCount.startPos.x + ( settings.guns[player.gun].ammoIconSpacing * ( i - 1 ))
@@ -194,6 +238,8 @@ end
 function scene:show( event )
   if ( event.phase == "did" ) then
     physics.start()
+    -- START SPAWNING ENEMIES CHANGE THIS NOT FUTURE PROOF
+    timer.performWithDelay( settings.game.spawnTime, spawnEnemy, -1 )
 	end
 end
 
